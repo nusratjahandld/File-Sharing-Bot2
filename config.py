505c851277e5b93e18613fa6afc9f80a7a28da8d
nusratjahandld <hridoyforbusiness@gmail.com>
2025-02-05 00:57:42 +0600

    Update config.py

--- config.py
+++ config.py
@@ -5,21 +5,21 @@ from logging.handlers import RotatingFileHandler
 
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5426922715:AAGOhaCp7aisT2E0_wSDUY2hY6bqIqvgm-g")
-API_ID = int(os.environ.get("API_ID", "17822592"))
-API_HASH = os.environ.get("API_HASH", "a20b3dbbe07ed695563b4609a3e62012")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = int(os.environ.get("API_ID", "21323689"))
+API_HASH = os.environ.get("API_HASH", "d2de82f692d5d2f7bf6abaa77868c6b8")
 
 
-OWNER_ID = int(os.environ.get("OWNER_ID", "578811855"))
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://temaga6324:idol813@demofile.dflkh.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
+OWNER_ID = int(os.environ.get("OWNER_ID", "5614482156"))
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://mamalinkde:KsDrxVEW183XtL91@cluster0.nbhtz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "mamalinkde")
 
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002153652898"))
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002153652898"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002417605133"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002359827838"))
 
 
-FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "600")) # auto delete in seconds
+FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "300")) # auto delete in seconds
 
 
 PORT = os.environ.get("PORT", "8080")
@@ -44,7 +44,7 @@ except ValueError:
 
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
 
 DISABLE_CHANNEL_BUTTON = True if os.environ.get('DISABLE_CHANNEL_BUTTON', "True") == "True" else False
 
@@ -56,11 +56,11 @@ BOT_STATS_TEXT = "<b>BOT UPTIME :</b>\n{uptime}"
 
 
 
-USER_REPLY_TEXT = "❌Don't Send Me Messages Directly I'm Only File Share Bot !"
+USER_REPLY_TEXT = "❌Don't Send Me Messages Directly I'm Only File Share Bot By @DostLinkDe!"
 
-START_MSG = os.environ.get("START_MESSAGE", "Hello {mention}\n\nI Can Store Private Files In Specified Channel And Other Users Can Access It From Special Link.")
+START_MSG = os.environ.get("START_MESSAGE", "Hello {mention}\n\nI Can Store Private Files In Specified Channel And Other Users Can Access It From Special Link. Visit @DostLinkDe For More Details About Us.")
 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n<b>You Need To Join In My Channel/Group To Use Me\n\nKindly Please Join Channel</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n<b>You Need To Join In Our Channel To Use Me\n\nKindly Please Join Channel</b>")
 
 
 
